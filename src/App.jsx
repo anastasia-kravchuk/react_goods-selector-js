16a683182b37773a06d83e68d8592743807d45e4
Anastasia <ahinata1717@gmail.com>
2025-10-02 22:39:51 +0300

    fix ternary logic

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,18 @@ export const goods = [
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
 
+  const handleSelectGood = good => {
+    setSelectedGood(good);
+  };
+
+  const handleRemoveSelection = () => {
+    setSelectedGood('');
+  };
+
+  const handleClearSelection = () => {
+    setSelectedGood('');
+  };
+
   return (
     <main className="section container">
       <h1 className="title is-flex is-align-items-center">
@@ -28,7 +40,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood('')}
+            onClick={handleClearSelection}
           />
         )}
       </h1>
@@ -49,7 +61,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood('')}
+                    onClick={handleRemoveSelection}
                   >
                     -
                   </button>
@@ -58,7 +70,7 @@ export const App = () => {
                     data-cy="AddButton"
                     type="button"
                     className="button"
-                    onClick={() => setSelectedGood(good)}
+                    onClick={() => handleSelectGood(good)}
                   >
                     +
                   </button>
